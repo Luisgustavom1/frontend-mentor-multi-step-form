2a1d0aa2a4b63aa3aff7556c28cb1eb8d4a9ef95
luisgustavom1 <luisgustavomacedo13@gmail.com>
2023-10-01 11:42:53 -0300

    feat: initial tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+import theme from "./theme"
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/*.{html,js}"
+  ],
+  theme: {
+    extend: {
+      ...theme
+    },
+  },
+  plugins: [],
+}
+
---
+++ theme.js
@@ -0,0 +1,31 @@
+module.exports = {
+  screens: {
+    sm: "375px",
+    md: '768px',
+    lg: '976px',
+    xl: '1440px',
+  },
+  colors: {
+    primary: {
+      "marine-blue": "hsl(213, 96%, 18%)",
+      'purplish-blue': "hsl(243, 100%, 62%)",
+      'pastel-blue': "hsl(228, 100%, 84%)",
+      'light-blue': "hsl(206, 94%, 87%)",
+      'strawberry-red': "hsl(354, 84%, 57%)",
+    },
+    neutral: {
+      'cool-gray': "hsl(231, 11%, 63%)",
+      'light-gray': "hsl(229, 24%, 87%)",
+      'magnolia': "hsl(217, 100%, 97%)",
+      'alabaster': "hsl(231, 100%, 99%)",
+      'white': "hsl(0, 0%, 100%)",
+    }
+  },
+  backgroundImage: {
+    'aside-mobile': "url('/src/assets/images/bg-sidebar-mobile.svg')",
+    'aside-desktop': "url('/src/assets/images/bg-sidebar-desktop.svg')",
+  },
+  backgroundSize: {
+    'full': '100%',
+  }
+}
